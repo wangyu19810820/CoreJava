4ad782d1c5018bbb5386a4d63176a9f47b31c196
wangyu19810820 <gadfly0036_cn@sina.com>
2020-06-20 20:45:29 +0800

    反射

---
+++ volume1/src/chapter05/CopyOfTest.java
@@ -0,0 +1,37 @@
+package chapter05;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class CopyOfTest {
+    public static void main(String[] args) {
+        int[] a = {1, 2, 3};
+        a = (int[])goodCopyOf(a, 10);
+        System.out.println(Arrays.toString(a));
+
+        String[] b = {"Tom", "Dick", "Marry"};
+        b = (String[])goodCopyOf(b, 10);
+        System.out.println(Arrays.toString(b));
+
+        // 抛异常ClassCastException
+//        b = (String[])badCopyOf(b, 10);
+    }
+
+    public static Object[] badCopyOf(Object[] a, int newLength) {
+        var newArray = new Object[newLength];
+        System.arraycopy(a, 0, newArray, 0, Math.min(a.length, newLength));
+        return newArray;
+    }
+
+    public static Object goodCopyOf(Object a, int newLength) {
+        Class cl = a.getClass();
+        if (!cl.isArray()) {
+            return null;
+        }
+        Class componentType = cl.getComponentType();
+        int length = Array.getLength(a);
+        Object newArray = Array.newInstance(componentType, newLength);
+        System.arraycopy(a, 0, newArray, 0, Math.min(length, newLength));
+        return newArray;
+    }
+}
