f45ff86b9beafe49d8d086258bc531903dbb5626
wangyu19810820 <gadfly0036_cn@sina.com>
2020-06-24 16:50:47 +0800

    thread

---
+++ volume1/src/chapter12/SyncBankTest.java
@@ -0,0 +1,69 @@
+package chapter12;
+
+import java.util.Arrays;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class SyncBankTest {
+    public static final int NACCOUNTS = 100;
+    public static final double INITIAL_BALANCE = 1000;
+    public static final double MAX_AMOUNT = 1000;
+    public static final int DELAY = 10;
+
+    public static void main(String[] args) {
+        var bank = new Bank2(NACCOUNTS, INITIAL_BALANCE);
+        for (int i = 0; i < NACCOUNTS; i++) {
+            int fromAccount = i;
+            Runnable r = () -> {
+                try {
+                    while (true) {
+                        int toAccount = (int)(bank.size() * Math.random());
+                        double amount = MAX_AMOUNT * Math.random();
+                        bank.transfer(fromAccount, toAccount, amount);
+                        Thread.sleep((int)(DELAY * Math.random()));
+                    }
+                } catch (InterruptedException e) {
+                }
+            };
+            var t = new Thread(r);
+            t.start();
+        }
+    }
+}
+
+class Bank2 {
+    private final double[] accounts;
+    private ReentrantLock bankLock = new ReentrantLock();
+
+    public Bank2(int n, double initialBalance) {
+        accounts = new double[n];
+        Arrays.fill(accounts, initialBalance);
+    }
+
+    public void transfer(int from, int to, double amount) {
+        bankLock.lock();
+        try {
+            if (accounts[from] < amount) {
+                return;
+            }
+            System.out.print(Thread.currentThread());
+            accounts[from] -= amount;
+            System.out.printf(" %10.2f from %d to %d", amount, from, to);
+            accounts[to] += amount;
+            System.out.printf(" Total Balance: %10.2f%n", getTotalBalance());
+        } finally {
+            bankLock.unlock();
+        }
+    }
+
+    public double getTotalBalance() {
+        double sum = 0;
+        for (double a : accounts) {
+            sum += a;
+        }
+        return sum;
+    }
+
+    public int size() {
+        return accounts.length;
+    }
+}
